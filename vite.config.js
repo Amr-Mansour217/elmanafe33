832389b83b14590aa3a3cc56c40f24996a768a04
Amr Mansour <amrmansour217@gmail.com>
2025-03-10 23:20:02 +0200

    add alias resolution and output directory configuration in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/elmanafe33/' // 👈 تأكد من كتابة اسم المستودع بشكل صحيح
+  resolve: {
+    alias: {
+      '@': './src'
+    }
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
