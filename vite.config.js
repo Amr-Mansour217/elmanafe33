74b2c52f600095675034e2c3a2b570013717a27a
Amr Mansour <amrmansour217@gmail.com>
2025-03-10 23:06:38 +0200

    update homepage URL and Vite base path to reflect project name change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/elmanafe33/' // 👈 تأكد من كتابة اسم المستودع بشكل صحيح
 })
