1a44990abeb11c7465a96cd6d6c237d9fe2b51d4
Amr Mansour <amrmansour217@gmail.com>
2025-03-11 00:21:49 +0200

    refactor routing to use wouter's Switch and hash location for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from "wouter";
+import { Router, Route, Switch } from "wouter";
+import useHashLocation from "wouter/use-hash-location";
 import Home from './components/home';
 import Videos from './components/video';
 import Quran from './components/quran';
@@ -10,18 +11,20 @@ import Apps from './components/apps';
 import Another from './components/another';
 
 function App() {
+  const base = import.meta.env.BASE_URL;
+  const useBasename = (hook) => (args) => hook(args.map(arg => base + arg));
   
   return (
-    <Router basename={import.meta.env.BASE_URL}> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/videos" element={<Videos />} />
-        <Route path="/quran" element={<Quran />} />
-        <Route path = "/library" element = {<Intre />} />
-        <Route path = "/bigarabicquran" element = {<Pdf />} />
-        <Route path = "/apps" element = {<Apps />} />
-        <Route path = "/anotherweb" element = {<Another />} />
-      </Routes>
+    <Router hook={useBasename} location={useHashLocation()}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/videos" component={Videos} />
+        <Route path="/quran" component={Quran} />
+        <Route path="/library" component={Intre} />
+        <Route path="/bigarabicquran" component={Pdf} />
+        <Route path="/apps" component={Apps} />
+        <Route path="/anotherweb" component={Another} />
+      </Switch>
     </Router>
   );
 };   
